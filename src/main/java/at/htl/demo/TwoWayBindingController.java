d43f0babb2f9f90f6a009e1666d968ff7a15888a
Jonas Dorfinger <jonas.dorfinger@gmx.at>
2020-03-06 10:19:51 +0100

    add setOnAction for previous Button

--- src/main/java/at/htl/demo/TwoWayBindingController.java
+++ src/main/java/at/htl/demo/TwoWayBindingController.java
@@ -2,8 +2,10 @@ package at.htl.demo;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -15,10 +17,21 @@ public class TwoWayBindingController implements Initializable {
     @FXML
     TextArea secondTextArea;
 
+    @FXML
+    Button previousButton;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-    secondTextArea.setText("");
+        secondTextArea.setText("");
 
-    secondTextArea.textProperty().bindBidirectional(textArea.textProperty());
+        secondTextArea.textProperty().bindBidirectional(textArea.textProperty());
+
+        previousButton.setOnAction(actionEvent -> {
+            try {
+                App.setRoot("OneWayBinding");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
 }
